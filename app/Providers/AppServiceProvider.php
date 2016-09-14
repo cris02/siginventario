94da0a12f37e70cb1678bb6fe00f2a9f1b6f529e
@alicia12 <alicemenjivar@hotmail.es>
2016-09-13 22:41:28 -0600

    modificacion2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,7 +6,7 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
+    /**  modifico comentario solo para saber si se guarda lo que hago xD
      * Bootstrap any application services.
      *
      * @return void
