1f75a85027f26b0ce90ac6ca8546c52e9093f4ab
Borja <mtbguason@hotmail.com>
2016-09-17 19:01:29 -0600

    provider aun no validado

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,17 +12,17 @@
 */
 
 
-Route::get('/', 'HomeController@index');
+
 
 
 Route::group(['middleware'=>['web']], function(){
 
+Route::get('/', 'HomeController@index');
 Route::get('home', 'HomeController@index');
 
 Route:: resource('menu','Menu\MenuController');
 Route:: resource('articulo','Article\ArticleController');
 
-Route::post('proveedor/store', 'Article\ProviderController@store');
 Route:: resource('proveedor','Article\ProviderController');
 
 
---
+++ resources/views/Msj/messages.blade.php
@@ -0,0 +1,23 @@
+@if(Session::has('save'))
+	
+	<div class="alert alert-success" role='alert'>
+		<strong>{{Session::get('save')}}</strong>		
+	</div>
+
+@endif
+
+@if(Session::has('update'))
+	
+	<div class="alert alert-success" role='alert'>
+		<strong>{{Session::get('update')}}</strong>		
+	</div>
+
+@endif
+
+@if(Session::has('delete'))
+	
+	<div class="alert alert-success" role='alert'>
+		<strong>{{Session::get('delete')}}</strong>		
+	</div>
+
+@endif  
